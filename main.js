15888dc66f7a3d2d823bb6c1d3506276b37604a0
kseckman <kseckm1@gmail.com>
2025-01-18 18:20:35 -0500

    first commit, added bbasic hmtl css and js function names

---
+++ main.js
@@ -0,0 +1,24 @@
+const myLibary = [];
+
+function book(){
+    //constructor
+    this.title
+    this.author
+    this.pages
+}
+
+book.prototype.read = function(){
+    console.log('read or not')
+    return `${this.title} by ${this.author}, ${this.pages}. has ${hasRead} been read.`
+}
+
+function addBookToLibrary(){
+    //do stuff
+
+}
+
+function displayBooks(){
+    myLibrary.map(book => {
+
+    })
+}
