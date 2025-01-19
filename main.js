650cccf65496bf26a5d50a7ea221a67e57ef2832
kseckman <kseckm1@gmail.com>
2025-01-19 18:11:26 -0500

    full functionality

--- main.js
+++ main.js
@@ -5,38 +5,75 @@ const hasRead = document.querySelector('#has-read')
 const submitBtn = document.querySelector('button')
 
 const myLibary = [];
-
 submitBtn.addEventListener('click', addBookToLibrary)
 
+
 function Book(title, author, pages,read){
-    //constructor
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
 
-//the method in the book constructor
-// Book.prototype.read = function(title, author, pages){
-//     console.log('read or not')
-//     return `${title} by ${author}, ${pages}. has ${hasRead} been read.`
-// }
+Book.prototype.toggleRead = function(){
+    this.read = !this.read
+}
 
 function addBookToLibrary(e){
-    //add new book to existing library.
     e.preventDefault() 
-    title = addTitle.value
-    author = addTitle.value
-    pages = addTitle.value
-    read = hasRead.checked
-    let book = new Book(title,author,pages,read)
-    myLibary.push(book)
-    console.log(myLibary)
+    title = addTitle.value;
+    author = addAuthor.value;
+    pages = addPages.value;
+    read = hasRead.checked;
+    let book = new Book(title,author,pages,read);
+    myLibary.push(book);
+    console.log(myLibary);
+    displayBooks(book);
+    addTitle.value = ''
+    addAuthor.value= ''
+    addPages.value = ''
+    hasRead.checked = ''
+  
 }
 
 function displayBooks(){
-    //loop through library , need to make card for each book. insert title author and pages to display
-    myLibrary.map(book => {
+    const bookList = document.querySelector('.book-list')
+    bookList.innerHTML ='';
 
+    myLibary.map(book => {
+        let card = document.createElement('div')
+        card.classList.add('book-card')
+        let cardH3 = document.createElement('h3')
+        cardH3.classList.add('book-title')
+        let cardAuthor = document.createElement('p')
+        cardAuthor.classList.add('book-author')
+        let cardPages = document.createElement('p')
+        cardPages.classList.add('book-pages')
+        let cardRead = document.createElement('p')
+        cardRead.classList.add('book-has-read')
+        let readBtn = document.createElement('button')
+        readBtn.classList.add('read-btn')
+        let deleteBtn = document.createElement('button')
+        deleteBtn.classList.add('delete-btn')
+
+        cardH3.textContent = `${book.title}`;
+        cardAuthor.textContent = `${book.author}`;
+        cardPages.textContent = `${book.pages}`;
+        cardRead.textContent = `${book.read ? 'read': 'unread'}`;
+        readBtn.textContent = 'read?';
+        deleteBtn.textContent = 'delete';
+
+        deleteBtn.addEventListener('click', removeBook)
+        readBtn.addEventListener('click', ()=>{
+            book.toggleRead();
+            displayBooks();
+        })
+
+        card.append(cardH3, cardAuthor, cardPages, cardRead,readBtn, deleteBtn)
+        bookList.append(card)
     })
+}
+
+function removeBook(){
+    this.parentElement.remove()
 }
